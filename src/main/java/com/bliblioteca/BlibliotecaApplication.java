e476c72ac99232f481320468273d71650b62c602
leo <leonardolucasga@hotmail.com>
2023-08-24 14:24:55 -0300

    Commit initial

--- src/main/java/com/bliblioteca/BlibliotecaApplication.java
+++ src/main/java/com/bliblioteca/BlibliotecaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlibliotecaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlibliotecaApplication.class, args);
+		SpringApplication.run(git remoBlibliotecaApplication.class, args);
 	}
 
 }
